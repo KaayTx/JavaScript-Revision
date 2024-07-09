9fd2425a86330c4f0464c7bd6c5f8beccc624a1c
KaayTx <kawtar.dahman@student.vinci.be>
2024-07-09 01:42:44 +0100

    2.2 : HMI : dom interaction

--- exercices/2.2/index.js
+++ exercices/2.2/index.js
@@ -7,11 +7,11 @@ message.innerHTML = 'Cliquez sur le bouton pour commencer à compter !';
 let count = 0;
 
 counter.addEventListener('click', () => {
-    counting.innerHTML = count;
     count++;
+    counting.innerHTML = count;
     if (count >= 5 && count <= 9) {
         message.textContent = 'Bravo, bel échauffement !';
     } else if (count >= 10) {
         message.textContent = 'Vous êtes passé maître en l\'art du clic !';
     }
-    });
+});
