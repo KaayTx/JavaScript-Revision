87742489dbb5ce9b02310f5dc3085e8f5c826766
KaayTx <kawtar.dahman@student.vinci.be>
2024-07-18 02:21:56 +0100

    2.11.1 : router & static pages

---
+++ exercices/2.11/src/Components/Pages/ViewMoviePage.js
@@ -0,0 +1,8 @@
+import {clearPage, renderPageTitle} from '../../utils/render';
+
+const ViewMoviePage = () => {
+    clearPage();
+    renderPageTitle('View Movie');
+}
+
+export default ViewMoviePage;
