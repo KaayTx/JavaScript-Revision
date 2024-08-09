a5d41ffcca8bfdd1d5b5e1f741f67a0e0ced32b5
KaayTx <kawtar.dahman@student.vinci.be>
2024-08-09 17:00:45 +0100

    2.20.1 : user session & theme

---
+++ exercices/2.20.2/frontend/src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+import Navbar from './Components/Navbar/Navbar';
+import Router from './Components/Router/Router';
+import Header from './Components/Header/Header';
+
+Header();
+Navbar();
+Router();
